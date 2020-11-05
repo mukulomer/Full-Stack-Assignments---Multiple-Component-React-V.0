d79345f91d3e0ffb3b8705fcc5b9748fe18c194b
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-11-05 21:30:04 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h2 data-ns-test="project-name">Book My Show Clone </h2>
+        <p data-ns-test="project-description">
+          this project is clone of book my show website .which is provide you
+          facility to book seat in theater or any show .{" "}
+        </p>
+      </>
+    );
+  }
 }
-
 
 export default App;
